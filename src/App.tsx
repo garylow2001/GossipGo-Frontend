b0921127b6d684b47456f056745d4c814fe1b8a1
Gary <garylow2001@gmail.com>
2024-01-03 18:06:50 +0800

    Store auth token as HTTP-only cookie

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,23 @@
 import React from 'react';
 import './App.css';
 import ThreadListView from './components/ThreadListView';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 
 import CreateThreadPage from './pages/CreateThreadPage';
 import ViewThreadPage from './pages/ViewThreadPage';
+import LoginPage from './pages/LoginPage';
 
 const App = () => {
   return (
     <Router>
       <Routes>
+        <Route index element={<ThreadListView />} />
         <Route path="/threads">
-          <Route index element={<ThreadListView />} />
           <Route path="/threads" element={<ThreadListView />} />
           <Route path="/threads/new-thread" element={<CreateThreadPage />} />
           <Route path="/threads/:id" element={<ViewThreadPage />} />
         </Route>
+          <Route path="/auth/login" element={<LoginPage />} />
       </Routes>
     </Router>
   );
