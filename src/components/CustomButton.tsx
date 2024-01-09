4dec91baba6a23534dfc03500f8b4986f646fb99
Gary <garylow2001@gmail.com>
2024-01-09 22:36:14 +0800

    Started tailwindcss

---
+++ src/components/CustomButton.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { cva } from 'class-variance-authority'
+
+const buttonStyles = cva(["hover:bg-secondary-hover"])
+
+const CustomButton = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default CustomButton
---
+++ src/layouts/PageHeader.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const PageHeader = () => {
+  return (
+    <div className='flex gap-10 lg:gap-20 justify-between'>
+        <div className='flex gap-4 items-center flex-shrink-0'>
+            IMAGE HERE
+        </div>
+    </div>
+  )
+}
+
+export default PageHeader
